f83025b091dff958659220177b5519f24fb6e482
Mikimos27 <mikimod27@gmail.com>
2025-10-16 08:25:21 +0200

    poprawki

--- L1/c/euklides.c
+++ L1/c/euklides.c
@@ -7,17 +7,8 @@ void swap(uint32_t* A, uint32_t* B){
     *B = temp;
 }
 
-uint32_t eukl1(uint32_t a, uint32_t b){
-    while(b != 0){
-        if(a < b) swap(&a, &b);
-        else a -= b;
-    }
-    return a;
-}
-
-
-uint32_t eukl2(uint32_t a, uint32_t b){
-    if(a < b) swap(&a, &b);
+uint32_t eukl(uint32_t a, uint32_t b){
+    if(a < b) eukl(b, a);
 
     while(b != 0){
         a = a % b;
@@ -33,10 +24,14 @@ int main(){
     scanf("%u", &a);
     scanf("%u", &b);
     if(a == 0 || b == 0) {
-        printf("NWD(%d, %d) = %d\n", a, b, a + b);
+        if(a + b == 0){
+            printf("NWD(0, 0) nie istneje\n");
+            return 1;
+        }
+        printf("NWD(%u, %u) = %u\n", a, b, a + b);
         return 1;
     }
-    printf("Euk1 = %d\nEuk2 = %d\n", eukl1(a, b), eukl2(a, b));
+    printf("NWD(%u, %u) = %u\n", a, b, eukl(a, b));
 
     return 0;
 }
