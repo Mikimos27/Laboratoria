1b5c4910be6a3cca42d5962e493612593a9e9ff5
Mikimos27 <mikimod27@gmail.com>
2025-10-24 21:12:14 +0200

    pierwsze pierwsze

---
+++ L2/c/prime.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+
+int test();
+int prime(uint32_t);
+
+
+int main(){
+    return test();
+}
+
+
+int test(){
+    uint32_t to_check[] = {0,2,24,17,3,1023,1204,1203};
+    for(int i = 0; i < sizeof(to_check) / sizeof(to_check[0]); i++){
+        printf("%d is%s prime\n", to_check[i], prime(to_check[i]) ? "" : " not");
+    }
+    return 0;
+}
+
+
+
+int prime(uint32_t n){
+    if(n <= 1) return 0;
+    if(n == 2) return 1;
+    if(n % 2 == 0) return 0;
+    uint32_t sq = (uint32_t)sqrt(n) + 1;
+    for(uint32_t i = 3; i < sq; i += 2){
+        if(n % i == 0) return 0;
+    }
+    return 1;
+}
